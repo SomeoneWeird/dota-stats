7c163ee91d9386e67038265bd38acade8b630e5e
Curia <admin@curiadesign.net>
2021-02-18 17:01:46 +1100

    Added better tailwind styled support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      width: {
-        '2px': '2px'
-      }
-    }
+    extend: {}
   },
   variants: {
     extend: {}
